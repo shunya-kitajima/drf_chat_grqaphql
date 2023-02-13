5c5cde363c33196fe4ff1544b175dabea4d8adb0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-14 01:01:30 +0900

    add mutate

--- bookstore/books/schema.py
+++ bookstore/books/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.forms.mutation import DjangoModelFormMutation
 from .models import Book, Author
-from .forms import CreateBookForm
+from .forms import CreateBookForm, CreateNewBookForm
 
 
 class BookType(DjangoObjectType):
@@ -19,6 +19,27 @@ class AuthorType(DjangoObjectType):
         return f"{self.last_name} {self.first_name}"
 
 
+class NewBookInput(graphene.InputObjectType):
+    title = graphene.String(required=True)
+    price = graphene.Int(required=True)
+    book_size_id = graphene.UUID(required=True)
+    author_id = graphene.UUID(required=True)
+
+
+class CreateBookMutation(graphene.Mutation):
+    class Meta:
+        output = BookType
+
+    class Arguments:
+        new_book_input = NewBookInput(required=True)
+
+    def mutate(self, info, new_book_input: NewBookInput):
+        form = CreateNewBookForm(data=new_book_input)
+        if form.is_valid():
+            book = form.save()
+            return book
+
+
 class Query:
     books = graphene.List(
         graphene.NonNull(BookType),
@@ -31,11 +52,6 @@ class Query:
         if title:
             qs = qs.filter(title__contains=title)
         return qs
-
-
-class CreateBookMutation(DjangoModelFormMutation):
-    class Meta:
-        form_class = CreateBookForm
 
 
 class Mutation(graphene.ObjectType):
