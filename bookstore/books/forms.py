d6c4f979faa51937b63a6194ab3e37546b04d542
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-14 00:56:00 +0900

    create CreateNewBookForm class

--- bookstore/books/forms.py
+++ bookstore/books/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from .models import Book
+from .models import Book, BookSize, Author
 
 
 class CreateBookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ("title", "price", "book_size_id")
+
+
+class CreateNewBookForm(forms.Form):
+    title = forms.CharField(required=True)
+    price = forms.IntegerField(required=True)
+    book_price_id = forms.ModelChoiceField(BookSize.objects, required=True)
+
+    def save(self):
+        return Book.objects.create(
+            title=self.cleaned_data["title"],
+            price=self.cleaned_data["price"],
+            book_id=self.cleaned_data["book_id"],
+        )
+
+
