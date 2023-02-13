281e7e843573f65f2e5f4e2cbd35fcbe0c45d95e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-02-13 17:06:14 +0900

    create classes

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class BookSize(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=30, blank=False)
+    height = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)
+
+
+class Book(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    title = models.CharField(max_length=30, blank=False)
+    price = models.IntegerField(default=0)
+    book_size_id = models.ForeignKey(
+        BookSize,
+        related_name="book_size_id",
+    )
+
+
+class Author(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    middle_name = models.CharField(max_length=30, blank=True)
+
+
+class BookAuthor(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    book_id = models.ForeignKey(
+        Book,
+        related_name="book_id",
+        on_delete=models.CASCADE,
+    )
+    author_id = models.ForeignKey(
+        Author,
+        related_name="author_id",
+        on_delete=models.CASCADE,
+    )
